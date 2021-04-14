2ecf80f812de356c544fda0b0ad4cf36190a3d35
hecariv <hector.casr1992@gmail.com>
2021-04-14 20:58:18 +0200

    fix bug

--- src/main/java/com/galvanize/autos/model/Automobile.java
+++ src/main/java/com/galvanize/autos/model/Automobile.java
@@ -14,7 +14,7 @@ public class Automobile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    private Long id;
     @Column(name = "model_year")
     private int year;
     private String make;
